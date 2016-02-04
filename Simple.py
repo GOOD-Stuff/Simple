250611eee91d7510229a7b33832ed178bc29fb0a
Gustoff <pomogite1310@mail.ru>
2016-02-05 02:24:56 +0300

    5.02.16. In DB is added to the file name, author's name, and description

--- Simple.py
+++ Simple.py
@@ -1,29 +1,28 @@
 import os
 import sqlite3
-from flask import (Flask, request, session, g, redirect, url_for, \
+from flask import (Flask, request, g, redirect, url_for, \
      abort, render_template, flash)
+from contextlib import closing
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
-from flask_uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
-from contextlib import closing
 
-#TODO: Add Flask-Upload?
-#TODO: Add Flask-Login?
-#TODO: Add Flask-Cache?
+#from flask.ext.uploads import UploadSet,configure_uploads, IMAGES, UploadNotAllowed
+#from flask_uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
 
 # Config
-DATABASE = '/tmp/simple.db'
-DEBUG = True
+DATABASE = '/tmp/upld.db'
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
-UPLOAD_FOLDER = '/tmp/uploads'
+
+UPLOAD_FOLDER = '/tmp/upload'
 ALLOWED_EXTENSIONS = set(['jpg','png', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.from_object(__name__)
+#app.config.from_object(__name__)
 
-### Enable database ###
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
@@ -42,47 +41,24 @@ def teardown_request(exception):
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
-#######################
-
-#class Photo:
-
-
-# Uploads
-#uploaded_photos = UploadSet('photos', IMAGES)
-#configure_uploads(app,uploaded_photos)
-
-# Flask-Upload style
-#@app.route('/upload', methods=['GET', 'POST'])
-#def upload():
-#    if request.method == 'POST' and 'photo' in request.files:
-#        filename = uploaded_photos.save(request.files['photo'])
-#        rec = Photo(filename=filename, user=g.user.id)
-#        rec.store()
-#        flash("Photo saved.")
-#        return redirect(url_for('show', id=rec.id))
-#    return render_template('upload.html')
-
-#@app.route('/photo/<id>')
-#def show(id):
-#    photo = Photo.load(id)
-#    if photo is None:
-#        abort(404)
-#    url = photos.url(photo.filename)
-#    return render_template('show.html', url=url, photo=photo)
-################################################
 
 #Standard solve upload file
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+             #send_from_directory(app.config[UPLOAD_FOLDER],filename)
+            g.db.execute('insert into entries (photoPath, comm, author) values (?, ?,?)',
+                  [filename, request.form['comm'],request.form['author']])
+            g.db.commit()
+            flash('New photo was successfully posted')
             return redirect(url_for('uploaded_file',filename=filename))
     return render_template('upload_photo.html')
 
@@ -91,7 +67,16 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+@app.route('/')
+def home():
+    return 'Hello world!'
 
+# @app.route('/')
+# def show_entries():
+#     cur = g.db.execute('select title, text from entries order by id desc')
+#     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+#     return render_template('show_entries.html', entries=entries)
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
-  photo_name text not null,
+  photoPath text not null,
   comm text not null,
   author text not null
 );
